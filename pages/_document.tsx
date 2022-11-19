57e9c598f6a0843b34683fa53fe0e1b20614cf40
FrancoAguzzi <faguzzi3@gmail.com>
2022-11-19 00:39:29 -0300

    Updated dApp favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document<DocumentProps> {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
